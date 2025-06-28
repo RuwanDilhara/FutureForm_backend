602c0276f74d071df623133ddbfe3911e3cf7816
RuwanDilhara <roshansameera916@gmail.com>
2025-06-29 00:14:22 +0530

    created the UserService  interface

---
+++ src/main/java/org/icet/future_form/service/UserService.java
@@ -0,0 +1,22 @@
+package org.icet.future_form.service;
+
+import org.icet.future_form.dto.UserDTO;
+import org.icet.future_form.utill.enums.UserRole;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface UserService {
+    List<UserDTO>getAll();
+    UserDTO getUserById(Integer userId);
+    UserDTO getUserByUserName(String userName);
+    List<UserDTO> getUsersByName(String fullName);
+    List<UserDTO> getUsersByBirthDay(LocalDate birthday);
+    List<UserDTO> getUsersByCreateAt(LocalDate createDate);
+    List<UserDTO> getUsersByUpdateAt(LocalDate updateAt);
+    List<UserDTO> getUsersByRole(UserRole role);
+    List<UserDTO> getUsersByActiveStatus(boolean isActive);
+    UserDTO getUserByEmail(String email);
+    UserDTO getUserByContactNumber(String contactNumber);
+
+}
