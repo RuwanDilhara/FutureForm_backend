7c2997973fd12bc87686f717dd02e699b6321477
RuwanDilhara <roshansameera916@gmail.com>
2025-06-29 00:03:53 +0530

    created the UserServiceImpl  class

---
+++ src/main/java/org/icet/future_form/service/impl/UserServiceImpl.java
@@ -0,0 +1,80 @@
+package org.icet.future_form.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.icet.future_form.dto.UserDTO;
+import org.icet.future_form.repository.UserRepository;
+import org.icet.future_form.service.UserService;
+import org.icet.future_form.utill.enums.UserRole;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    final UserRepository repository;
+    final ModelMapper mapper;
+
+    @Override
+    public List<UserDTO> getAll() {
+        return repository.findAll().stream()
+                .map(entity -> mapper.map(entity,UserDTO.class)).toList();
+    }
+
+    @Override
+    public UserDTO getUserById(Integer userId) {
+        return mapper.map(repository.findById(userId),UserDTO.class);
+    }
+
+    @Override
+    public UserDTO getUserByUserName(String userName) {
+        return mapper.map(repository.findByUsername(userName),UserDTO.class);
+    }
+
+    @Override
+    public List<UserDTO> getUsersByName(String fullName) {
+        return repository.findByFullName(fullName).stream()
+                .map(entity -> mapper.map(entity,UserDTO.class)).toList();
+    }
+
+    @Override
+    public List<UserDTO> getUsersByBirthDay(LocalDate birthday) {
+        return repository.findByBirthday(birthday).stream()
+                .map(entity -> mapper.map(entity,UserDTO.class)).toList();
+    }
+
+    @Override
+    public List<UserDTO> getUsersByCreateAt(LocalDate createDate) {
+        return repository.findByCreateAt(createDate).stream()
+                .map(entity -> mapper.map(entity,UserDTO.class)).toList();
+    }
+
+    @Override
+    public List<UserDTO> getUsersByUpdateAt(LocalDate updateAt) {
+        return repository.findByUpdateAt(updateAt).stream()
+                .map(entity -> mapper.map(entity,UserDTO.class)).toList();
+    }
+
+    @Override
+    public List<UserDTO> getUsersByRole(UserRole role) {
+        return repository.findByRole(role).stream()
+                  .map(entity -> mapper.map(entity,UserDTO.class)).toList();
+    }
+
+    @Override
+    public List<UserDTO> getUsersByActiveStatus(boolean isActive) {
+        return repository.findByIsActive(isActive).stream()
+                .map(entity -> mapper.map(entity,UserDTO.class)).toList();
+    }
+
+    @Override
+    public UserDTO getUserByEmail(String email) {
+        return mapper.map(repository.findByEmail(email),UserDTO.class);
+    }
+
+    @Override
+    public UserDTO getUserByContactNumber(String contactNumber) {
+        return mapper.map(repository.findByContactNumber(contactNumber),UserDTO.class);
+    }
+}
