50a41a1c6a06e8b8650fd0471ed5c3b490916e60
RuwanDilhara <roshansameera916@gmail.com>
2025-06-29 00:52:56 +0530

    updated the user entity

--- src/main/java/org/icet/future_form/entity/User.java
+++ src/main/java/org/icet/future_form/entity/User.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.icet.future_form.utill.enums.UserRole;
+
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
@@ -16,4 +19,15 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
+
+    private String username;
+    private String fullName;
+    private String contactNumber;
+    private String email;
+    private UserRole role;
+    private LocalDate birthday;
+    private String password;
+    private LocalDate createAt;
+    private LocalDate updateAt;
+    private boolean isActive;
 }
